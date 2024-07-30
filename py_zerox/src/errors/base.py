ab5694929b17f89b5362569d026346f5ca8e1091
wizenheimer <nayan.kumar@couchbase.com>
2024-07-30 14:22:09 +0530

    feat: add custom exceptions

---
+++ py_zerox/src/errors/base.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+
+class CustomException(Exception):
+    def __init__(
+        self,
+        message: Optional[str] = None,
+        extra_info: Optional[dict] = None,
+    ):
+        self.message = message
+        self.extra_info = extra_info
+        super().__init__(self.message)
+
+    def __str__(self):
+        if self.extra_info:
+            return f"{self.message} (Extra Info: {self.extra_info})"
+        return self.message
---
+++ py_zerox/src/errors/exceptions.py
@@ -0,0 +1,52 @@
+from typing import Dict, Optional
+from src.constants.messages import Messages
+from src.errors.base import CustomException
+
+
+class MissingOpenAIAPIKeyException(CustomException):
+
+    def __init__(
+        self,
+        message: str = Messages.NO_OPENAI_KEY,
+    ):
+        super().__init__(message)
+
+
+class ResourceUnreachableException(CustomException):
+
+    def __init__(
+        self,
+        message: str = Messages.FILE_UNREACHAGBLE,
+        extra_info: Optional[Dict] = None,
+    ):
+        super().__init__(message, extra_info)
+
+
+class FileUnavailable(CustomException):
+
+    def __init__(
+        self,
+        message: str = Messages.FILE_PATH_MISSING,
+        extra_info: Optional[Dict] = None,
+    ):
+        super().__init__(message, extra_info)
+
+
+class FailedToSaveFile(CustomException):
+
+    def __init__(
+        self,
+        message: str = Messages.FAILED_TO_SAVE_FILE,
+        extra_info: Optional[Dict] = None,
+    ):
+        super().__init__(message, extra_info)
+
+
+class FailedToProcessFile(CustomException):
+
+    def __init__(
+        self,
+        message: str = Messages.FAILED_TO_PROCESS_IMAGE,
+        extra_info: Optional[Dict] = None,
+    ):
+        super().__init__(message, extra_info)
