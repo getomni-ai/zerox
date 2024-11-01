0dd29ac600e3b7f9df7aa0529b94ec2b51d24705
Zeeshan Zulfiqar Ali <zeeshan.dossani23@gmail.com>
2024-11-01 11:43:34 +0500

    Add testing mechanism (#82)
    * Add testing mechanism
    
    * expand on tests
    
    * editorconfig
    
    * rm duplicate from gitignore
    
    * comments
    
    ---------
    
    Co-authored-by: Tyler Maran <tyler.maran@gmail.com>

---
+++ node-zerox/tests/utils.ts
@@ -0,0 +1,39 @@
+export const compareKeywords = (
+  pages: { content: string }[],
+  expectedKeywords: string[][]
+) => {
+  const keywordCounts: {
+    keywordsFound: string[];
+    keywordsMissing: string[];
+    page: number;
+    totalKeywords: number;
+  }[] = [];
+
+  for (let i = 0; i < expectedKeywords.length; i++) {
+    const page = pages[i];
+    const keywords = expectedKeywords[i];
+    const keywordsFound: string[] = [];
+    const keywordsMissing: string[] = [];
+
+    if (page && keywords) {
+      const pageContent = page.content.toLowerCase();
+
+      keywords.forEach((keyword) => {
+        if (pageContent.includes(keyword.toLowerCase())) {
+          keywordsFound.push(keyword);
+        } else {
+          keywordsMissing.push(keyword);
+        }
+      });
+    }
+
+    keywordCounts.push({
+      keywordsFound,
+      keywordsMissing,
+      page: i + 1,
+      totalKeywords: keywords.length,
+    });
+  }
+
+  return keywordCounts;
+};
