7481055fd591a53709867ec4f0df02847b3c90bc
Tyler Maran <tyler.maran@gmail.com>
2024-12-09 16:02:53 -0800

    Also increase max tokens

--- node-zerox/src/utils.ts
+++ node-zerox/src/utils.ts
@@ -16,7 +16,7 @@ const convertAsync = promisify(convert);
 
 const defaultLLMParams: LLMParams = {
   frequencyPenalty: 0, // OpenAI defaults to 0
-  maxTokens: 2000,
+  maxTokens: 4000,
   presencePenalty: 0, // OpenAI defaults to 0
   temperature: 0,
   topP: 1, // OpenAI defaults to 1
