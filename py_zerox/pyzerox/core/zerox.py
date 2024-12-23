256ab4b649e11958c8a520c1618376e8877e02bf
brightertiger <brightertiger@users.noreply.github.com>
2024-12-23 12:38:08 +0530

    Update zerox.py

--- py_zerox/pyzerox/core/zerox.py
+++ py_zerox/pyzerox/core/zerox.py
@@ -133,7 +133,7 @@ async def zerox(
                                                  **subset_pdf_create_kwargs)
 
         # Convert the file to a series of images, below function returns a list of image paths in page order
-        images = await convert_pdf_to_images(image_density=image_density, image_height=image_height, local_path=local_path, temp_dir=temp_directory)
+        images = await convert_pdf_to_images(image_density=image_density, image_height=image_height, local_path=local_path, temp_dir=temp_directory, poppler_path=poppler_path)
 
         if maintain_format:
             for image in images:
