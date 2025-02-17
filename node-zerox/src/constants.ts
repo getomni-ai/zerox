ef4c53a5cbed945586747dfbd9aa2cf4c4a3657c
Anna <annapojawis@gmail.com>
2025-02-17 11:40:31 -0800

    Update system prompt

--- node-zerox/src/constants.ts
+++ node-zerox/src/constants.ts
@@ -7,7 +7,15 @@ export const CONSISTENCY_PROMPT = (priorPage: string): string =>
   `Markdown must maintain consistent formatting with the following page: \n\n """${priorPage}"""`;
 
 export const SYSTEM_PROMPT_BASE = `
-  Convert the following PDF page to markdown.
-  Return only the markdown with no explanation text. Do not include delimiters like '''markdown.
-  You must include all information on the page. Do not exclude headers, footers, or subtext.
+Convert the following document to markdown.
+Return only the markdown with no explanation text. Do not include delimiters like '''markdown or '''html.
+
+RULES:
+  - You must include all information on the page. Do not exclude headers, footers, or subtext.
+  - Return tables in an HTML format.
+  - Charts & infographics must be interpreted to a markdown format. Prefer table format when applicable.
+  - Logos should be wrapped in brackets. Ex: <logo>Coca-Cola<logo>
+  - Watermarks should be wrapped in brackets. Ex: <watermark>OFFICIAL COPY<watermark>
+  - Page numbers should be wrapped in brackets. Ex: <page_number>14<page_number> or <page_number>9/22<page_number>
+  - Prefer using ☐ and ☑ for check boxes.
 `;
--- py_zerox/pyzerox/constants/prompts.py
+++ py_zerox/pyzerox/constants/prompts.py
@@ -2,7 +2,15 @@ class Prompts:
     """Class for storing prompts for the Zerox system."""
 
     DEFAULT_SYSTEM_PROMPT = """
-    Convert the following PDF page to markdown.
-    Return only the markdown with no explanation text.
-    Do not exclude any content from the page.
+    Convert the following document to markdown.
+    Return only the markdown with no explanation text. Do not include delimiters like '''markdown or '''html.
+
+    RULES:
+      - You must include all information on the page. Do not exclude headers, footers, or subtext.
+      - Return tables in an HTML format.
+      - Charts & infographics must be interpreted to a markdown format. Prefer table format when applicable.
+      - Logos should be wrapped in brackets. Ex: <logo>Coca-Cola<logo>
+      - Watermarks should be wrapped in brackets. Ex: <watermark>OFFICIAL COPY<watermark>
+      - Page numbers should be wrapped in brackets. Ex: <page_number>14<page_number> or <page_number>9/22<page_number>
+      - Prefer using ☐ and ☑ for check boxes.
     """
